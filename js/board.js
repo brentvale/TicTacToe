cb990584f8ce93bdd1d3e0a27a0b16db04db64ca
Brent Vale <brentvale@gmail.com>
2016-09-30 09:28:31 -0700

    tic-tac-toe coding challenge

---
+++ js/board.js
@@ -0,0 +1,80 @@
+var Board = function(){
+  this.grid = [["-", "-", "-"],["-","-","-"],["-","-","-"]];
+  this.currentTurn = null;
+};
+
+Board.prototype = {
+  addMove: function(num){
+    var hash = {1: [0,0],
+                2: [0,1],
+                3: [0,2],
+                4: [1,0],
+                5: [1,1],
+                6: [1,2],
+                7: [2,0],
+                8: [2,1],
+                9: [2,2]};
+                
+    var indeces = hash[num];
+    this.grid[indeces[0]][indeces[1]] = this.currentTurn;
+  },
+  currentPlayerWon: function(){
+    //playerValue "X" or "O"
+    
+    //loop captures win for 3 across
+    for(var i = 0; i < 3; i++){
+      var allSame = true;
+      for(var j =0; j < 3; j++ ){
+        if(this.grid[i][j] !== this.currentTurn){
+          allSame = false;
+        }
+      }
+      if(allSame){
+        return true;
+      }
+    }
+    
+    //capture win for 3 down
+    for(var i = 0; i < 3; i++){
+      var allSame = true;
+      for(var j =0; j < 3; j++ ){
+        if(this.grid[j][i] !== this.currentTurn){
+          allSame = false;
+        }
+      }
+      if(allSame){
+        return true;
+      }
+    }
+    
+    //capture win for 2 diagonals
+    var diagOne = [[0,0], [1,1], [2,2]];
+    var diagTwo = [[2,0], [1,1], [0,2]];
+    
+    var diagOneSame = true;
+    for(var i = 0; i < 3; i ++){
+      if(this.grid[diagOne[i][0]][diagOne[i][1]] !== this.currentTurn){
+        diagOneSame = false;
+      }
+    }
+    if(diagOneSame){
+      return true;
+    }
+    
+    var diagTwoSame = true;
+    for(var i = 0; i < 3; i ++){
+      if(this.grid[diagTwo[i][0]][diagTwo[i][1]] !== this.currentTurn){
+        diagTwoSame = false;
+      }
+    }
+    if(diagTwoSame){
+      return true;
+    }
+    
+    return false;
+  },
+  setCurrentTurn: function(turn){
+    this.currentTurn = turn;
+  }
+  
+}
---
+++ js/game.js
@@ -0,0 +1,46 @@
+var Game = function(options){
+  this.playerOne = "X";
+  this.playerTwo = "O";
+  this.currentTurn = null;
+  this.board = new Board({board: options.board});
+  this.startGame();
+}
+
+Game.prototype = {
+  addEventListeners: function(){
+    var that = this;
+    $("#main div div").on("click", function(event){
+      var classNum = parseInt($(event.currentTarget).attr("class").slice(-1));
+
+      that.board.setCurrentTurn(that.currentTurn);
+      that.board.addMove(classNum);
+      
+      $(event.currentTarget).html(that.currentTurn);
+      that.togglePlayerTurn();
+    });
+  },
+  resetGame: function(){
+    var $board = $("#main");
+    var tiles = $board.find("[class*='tile-']");
+    for(var i = 0; i < tiles.length; i ++){
+      $(tiles[i]).html("");
+    }
+    $("#main div div").off("click");
+    this.board = new Board({board: $board});
+    this.startGame();
+  },
+  startGame: function(){
+    this.addEventListeners();
+    this.currentTurn = this.playerOne;
+    this.board.currentTurn = this.playerOne;
+  },
+  togglePlayerTurn: function(){
+    var playerHasWon = this.board.currentPlayerWon();
+    if(playerHasWon){
+      alert(this.currentTurn + " has Won the game!")
+      this.resetGame();
+    } else {
+      this.currentTurn = (this.currentTurn == this.playerOne) ? this.playerTwo : this.playerOne;
+    }
+  }
+}
